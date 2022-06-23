edb9ad1b986142a7f0c85be143aa59655789da3a
Smaug6739 <veauville.raphael@gmail.com>
2022-06-23 17:47:39 +0200

    Display log in loop (track infinity loop)

--- tokenizer/tokenizer_block/main.go
+++ tokenizer/tokenizer_block/main.go
@@ -1,9 +1,14 @@
 package tokenizer_block
 
-import "github.com/Smaug6739/mparser/preprocessor"
+import (
+	"github.com/Smaug6739/mparser/internal/logger"
+	"github.com/Smaug6739/mparser/preprocessor"
+)
 
 func New(state *preprocessor.Markdown) {
 	for state.Tokens[len(state.Tokens)-1].Line < state.TotalIndexes {
+		logger.New().Warn("Loop tokenizer main")
 		tokenizeBlockHeader(state)
+		tokenizeBlockParagraph(state)
 	}
 }
