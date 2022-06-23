f296b248fdbe8e7909a38888db9c43d110f6186b
Smaug6739 <veauville.raphael@gmail.com>
2022-06-23 18:37:34 +0200

    Clarify key name TotalIndexes => MaxIndex

--- preprocessor/prepar.go
+++ preprocessor/prepar.go
@@ -8,9 +8,9 @@ func prepar(str string) *Markdown {
 	// The main Markdown instance
 	lines := strings.Split(str, "\n")
 	var instance Markdown = Markdown{
-		Source:       str,
-		Lines:        lines,
-		TotalIndexes: len(lines) - 1,
+		Source:   str,
+		Lines:    lines,
+		MaxIndex: len(lines) - 1,
 		Tokens: Tokens{
 			Token{
 				Token:    "",
--- preprocessor/types.go
+++ preprocessor/types.go
@@ -1,10 +1,10 @@
 package preprocessor
 
 type Markdown struct {
-	Source       string   // The brute string
-	Lines        []string // The source splited on character "\n"
-	TotalIndexes int      // Total of indexes
-	Tokens       Tokens   // Tokens (see: Token)
+	Source   string   // The brute string
+	Lines    []string // The source splited on character "\n"
+	MaxIndex int      // Maximal index
+	Tokens   Tokens   // Tokens (see: Token)
 }
 
 type Tokens = []Token
