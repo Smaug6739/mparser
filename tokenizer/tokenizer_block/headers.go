2f0f0619167d90812f12e04385ebfff06d5f7c2a
Smaug6739 <veauville.raphael@gmail.com>
2022-06-24 09:24:23 +0200

    Optimizations and comments

--- tokenizer/tokenizer_block/headers.go
+++ tokenizer/tokenizer_block/headers.go
@@ -9,30 +9,32 @@ import (
 
 func tokenizeBlockHeader(state *preprocessor.Markdown) bool {
 
+	// Get common informations
 	data, err := getInfos(state)
 	if err != nil {
 		return false
 	}
 
-	// If the string start by more than 3 spaces, returns
+	// If the string start by more than 3 spaces (it should be a code-block), returns
 	leftTrimmed := strings.TrimLeft(data.lineContent, " ")
 	leadingSpaces := countLeadingSpaces(data.lineContent, leftTrimmed)
-
 	if leadingSpaces >= 4 {
 		return false
 	}
+
 	for level := 6; level >= 1; level-- {
 		prefix := strings.Repeat(" ", leadingSpaces) + strings.Repeat("#", level) // The markdown prefix
 
-		if strings.HasPrefix(data.lineContent, prefix) {
+		if strings.HasPrefix(data.lineContent, prefix) { // No space after # is allowed for empty headers
 			content := data.lineContent[len(prefix):] // The content based on prefix length
 			if strings.HasPrefix(data.lineContent, prefix+" ") {
 				prefix += " "                            // Increase the prefix with white space
 				content = data.lineContent[len(prefix):] // Update the content based on prefix
-			} else if content != "" {
-				return false // If there has no space *and* the content is not empty, returns
+			} else if len(content) > 0 { // If there has no space *and* the content is not empty, returns
+				return false
 			}
 
+			// All verifications pass, update state
 			state.Tokens = append(state.Tokens, preprocessor.Token{
 				Token:    "header_start",
 				Html:     `<h` + strconv.Itoa(level) + `>`,
@@ -42,7 +44,7 @@ func tokenizeBlockHeader(state *preprocessor.Markdown) bool {
 			})
 			state.Tokens = append(state.Tokens, preprocessor.Token{
 				Token:   "inline",
-				Content: strings.Trim(data.lineContent[len(prefix):], " "),
+				Content: strings.Trim(content, " "),
 				Line:    data.lineIndex,
 				Block:   false,
 			})
@@ -52,7 +54,7 @@ func tokenizeBlockHeader(state *preprocessor.Markdown) bool {
 				Line:  data.lineIndex,
 				Block: true,
 			})
-			return true
+			return true // The state was updated
 		}
 	}
 	return false
