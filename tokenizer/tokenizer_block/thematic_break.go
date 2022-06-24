34031fd707f5f73195fa0a244ab6f15da9536152
Smaug6739 <veauville.raphael@gmail.com>
2022-06-24 09:25:53 +0200

    Comment

---
+++ tokenizer/tokenizer_block/thematic_break.go
@@ -0,0 +1,11 @@
+package tokenizer_block
+
+import "github.com/Smaug6739/mparser/preprocessor"
+
+func tokenizeBlockThematicBreak(state *preprocessor.Markdown) {
+	/*data, err := getInfos(state)
+	if err != nil {
+		return
+	}*/
+
+}
